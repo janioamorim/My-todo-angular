07fed35960521977981997fd56d7a65cc5128940
José Roberto <joserobertobarbosabezerra@gmail.com>
2021-07-07 12:53:10 -0300

    commit jose

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ProductCreateComponent } from "./Components/Product/product-create/product-create.component";
+import { ProductDeleteComponent } from "./Components/Product/product-delete/product-delete.component";
+import { ProductUpdateComponent } from "./Components/Product/product-update/product-update.component";
 
 import { HomeComponent } from "./views/home/home.component";
 import { ProductComponent } from "./views/product/product.component";
@@ -14,7 +17,18 @@ const routes: Routes = [
     path: "products",
     component: ProductComponent
   },
-
+  {
+    path: "products/create",
+    component: ProductCreateComponent
+  },
+  {
+    path: "products/update/:id",
+    component: ProductUpdateComponent
+  },
+  {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent
+  },
 ];
 
 @NgModule({
