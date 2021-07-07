f20cf573688c0009b7ebf302e2c560c621d1f4c5
José Roberto <joserobertobarbosabezerra@gmail.com>
2021-07-07 16:56:45 -0300

    commit jose roberto

--- src/app/Components/Product/product-delete/product-delete.component.ts
+++ src/app/Components/Product/product-delete/product-delete.component.ts
@@ -18,16 +18,16 @@ export class ProductDeleteComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    //const id = +this.route.snapshot.paramMap.get('id');
-    //this.productService.readById(id).subscribe((product) => {
+   // const id = +this.route.snapshot.paramMap.get('id');
+   // this.productService.readById(id).subscribe((product) => {
     //  this.product = product;
-    //});
+   // });
   }
 
   deleteProduct(): void {
     //this.productService.delete(this.product.id).subscribe(() => {
      // this.productService.showMessage("Produto excluido com sucesso!");
-     // this.router.navigate(["/products"]);
+      //this.router.navigate(["/products"]);
    // });
   }
 
