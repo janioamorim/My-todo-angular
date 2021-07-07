e5641f9d17646d3175a6b91984e480717eb4f646
Ailton Oliveira <ailtondacosta-91@hotmail.com>
2021-07-07 17:39:22 -0300

    commit

--- src/app/Components/Product/product-delete/product-delete.component.ts
+++ src/app/Components/Product/product-delete/product-delete.component.ts
@@ -9,6 +9,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./product-delete.component.css"],
 })
 export class ProductDeleteComponent implements OnInit {
+
   product!: Product;
 
   constructor(
@@ -18,17 +19,17 @@ export class ProductDeleteComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.productService.readById(id).subscribe((product) => {
-      this.product = product;
-    });
+    //const id = this.route.snapshot.paramMap.get('id');
+    //this.productService.readById(id).subscribe((product) => {
+    //  this.product = product;
+    //});
   }
 
   deleteProduct(): void {
-    this.productService.delete(this.product.id).subscribe(() => {
-      this.productService.showMessage("Produto excluido com sucesso!");
-      this.router.navigate(["/products"]);
-    });
+    //this.productService.delete(this.product.id).subscribe(() => {
+    //  this.productService.showMessage("Produto excluido com sucesso!");
+   //   this.router.navigate(["/products"]);
+   // });
   }
 
   cancel(): void {
