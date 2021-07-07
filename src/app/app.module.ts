8979b3d8e2a7464aada93dff05b7e1def8f31cf1
José Roberto <joserobertobarbosabezerra@gmail.com>
2021-07-07 10:15:14 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//importação Dos componentes de visualização
 import { HeaderComponent } from './TEMPLATE/header/header.component';
 import { NavComponent } from './TEMPLATE/nav/nav.component';
+
+
+//importação dos componentes material
+
 
 @NgModule({
   declarations: [
