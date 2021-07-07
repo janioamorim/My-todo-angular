5280403f0c783d9eea9de141891f69cdb374dab5
José Roberto <joserobertobarbosabezerra@gmail.com>
2021-07-07 12:05:08 -0300

    commit jose roberto

--- src/app/views/product/product.component.ts
+++ src/app/views/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/Components/Template/header/header.service';
+
 
 @Component({
   selector: 'app-product',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos',
+      icon: 'storefront',
+      routeUrl: '/products'
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
   }
 
 }
