c6b2f0a2d117adda14224b20dc6f71d11c5c1874
José Roberto <joserobertobarbosabezerra@gmail.com>
2021-07-07 15:31:45 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/Template/header/header.component';
-import { NavComponent } from './Components/Template/nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//importação dos componentes
 import { HomeComponent } from './views/home/home.component';
 import { ProductComponent } from './views/product/product.component';
 import { ProductDeleteComponent } from './Components/Product/product-delete/product-delete.component';
 import { ProductReadComponent } from './Components/Product/product-read/product-read.component';
 import { ProductUpdateComponent } from './Components/Product/product-update/product-update.component';
 import { ProductCreateComponent } from './Components/Product/product-create/product-create.component';
+import { HeaderComponent } from './Components/Template/header/header.component';
+import { NavComponent } from './Components/Template/nav/nav.component';
 
 //importação dos componentes material
 import { MatToolbarModule } from '@angular/material/toolbar';
